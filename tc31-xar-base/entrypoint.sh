0733739dfae0de228fe62e14debfe36fb36cebb6
Alexander Hansson <alexander.hansson@ietv.se>
2026-03-08 15:23:00 +0100

    Append semicolon to if statement

--- tc31-xar-base/entrypoint.sh
+++ tc31-xar-base/entrypoint.sh
@@ -9,7 +9,7 @@ echo "Starting TcSystemServiceUm..."
 
 # Replaces the shell process with the TcSystemServiceUm process, ensuring proper signal handling
 # 0x4 is FastAsPossible, 0x7 should be realtime
-if ${REALTIME} then
+if ${REALTIME}; then
     exec /usr/bin/TcSystemServiceUm -f 0x7 -i "${AMS_NETID}" -p /var/run/TcSystemServiceUm.pid
 else
     exec /usr/bin/TcSystemServiceUm -f 0x4 -i "${AMS_NETID}" -p /var/run/TcSystemServiceUm.pid
